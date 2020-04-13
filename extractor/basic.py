908b5bf7f551dab06a2b6e62672a9a9ad77ba8f1
Somesh Kumar Singh <f20180175@goa.bits-pilani.ac.in>
2020-04-13 23:57:00 +0530

    Basic Skeleton Module

--- extractor/basic.py
+++ extractor/basic.py
@@ -1,7 +1,30 @@
 _AUTHOR_ = "SOMESH"
 
+import abc
 import random
 
+class ImportantWordExtractor(metaclass=abc.ABCMeta):
+    """
+        An abstract class used to represent the Keyword Extractor.SubClass should implement the extract method.
+        Methods
+        -------
+        extract(self, words: list, **kwargs)
+            - returns the important word's index.
+    """
 
-def get_random_word(ListOfWords):
-    return random.choice(ListOfWords)
+    @abc.abstractmethod
+    def extract(self, words: list, top_k: int, **kwargs):
+        """Applies perturbation and returns the word."""
+        raise NotImplementedError
+
+class RandomImportantWordExtractor(ImportantWordExtractor):
+    """
+        A class used to apply random word extractor.
+        Methods
+        -------
+        extract(self, word: list, **kwargs)
+            - applies the space perturbation on the word and returns it.
+    """
+
+    def extract(self, words: list, top_k=1, **kwargs):
+        return random.sample(range(len(words)), top_k)
