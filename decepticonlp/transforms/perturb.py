8c14f4c8369478921136a0cdf980eabca656c7d3
Somesh Kumar Singh <f20180175@goa.bits-pilani.ac.in>
2020-05-09 02:49:51 +0530

    Black Format

--- decepticonlp/transforms/perturb.py
+++ decepticonlp/transforms/perturb.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+
 # character level perturbations.
 def insert_space(word):
     """
@@ -8,8 +9,9 @@ def insert_space(word):
     Performs a single split and returns a list of two 
     new strings.
     """
-    index=random.randint(0,len(word)) #select random index
-    return word[:index]+' '+word[index:] #insert space
+    index = random.randint(0, len(word))  # select random index
+    return word[:index] + " " + word[index:]  # insert space
+
 
 def swap(word):
     """
@@ -17,33 +19,39 @@ def swap(word):
     intact the first and last letter. Naturally a string
     of size>=4 is required for this.
     """
-    charlist=list(word)
-    index = random.randint(1,len(word)-3) #select random offset for tuple
-    charlist[index],charlist[index+1]=charlist[index+1],charlist[index] #swap tuple
-    return ''.join(charlist)
+    charlist = list(word)
+    index = random.randint(1, len(word) - 3)  # select random offset for tuple
+    charlist[index], charlist[index + 1] = (
+        charlist[index + 1],
+        charlist[index],
+    )  # swap tuple
+    return "".join(charlist)
+
 
 def delete(word):
     """
     Deletes a random character from a word
     except for first and last character.
     """
-    index=random.randint(1,len(word)-2) #select random index
-    return word[:index]+word[index+1:] #delete index
+    index = random.randint(1, len(word) - 2)  # select random index
+    return word[:index] + word[index + 1 :]  # delete index
 
-def visual_similar_chars(word,*arg):
 
-    method_pick=np.random.randint(0,len(arg))   
-    if arg[method_pick]=='unicode': 
+def visual_similar_chars(word, *arg):
+
+    method_pick = np.random.randint(0, len(arg))
+    if arg[method_pick] == "unicode":
         """
         get diacritic  characters
-        """ 
-        char_array=np.array(list(word))
-        diacritic=np.char.add(char_array,u'\u0301')
+        """
+        char_array = np.array(list(word))
+        diacritic = np.char.add(char_array, u"\u0301")
         return diacritic
-    if arg[method_pick]=='visual':
+    if arg[method_pick] == "visual":
         """
         get visually similar chars. like @ for a. 0 for O.
         """
-        return None  
-print(visual_similar_chars('shashank','unicode','visual'))   
+        return None
 
+
+print(visual_similar_chars("shashank", "unicode", "visual"))
--- setup.py
+++ setup.py
@@ -4,45 +4,49 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Rajaswa Ravindra Patil",
-    author_email='rajp4480@gmail.com',
-    python_requires='>=3.5',
+    author_email="rajp4480@gmail.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Python package for Adversarial Debugging of Natural Language Models",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='decepticonlp',
-    name='decepticonlp',
-    packages=find_packages(include=['decepticonlp', 'decepticonlp.*']),
+    keywords="decepticonlp",
+    name="decepticonlp",
+    packages=find_packages(include=["decepticonlp", "decepticonlp.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/SforAiDl/decepticonlp',
-    version='0.1.0',
+    url="https://github.com/SforAiDl/decepticonlp",
+    version="0.1.0",
     zip_safe=False,
 )
