0392a03fa0a9c4a1ef8f7c68d504f4062e443aba
Parantak Singh <parantak.axilblaze@gmail.com>
2020-05-10 10:55:50 +0530

    Add typo perturbation
    shifts a character by one keyboard space

--- decepticonlp/transforms/perturb.py
+++ decepticonlp/transforms/perturb.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import math
 
 # character level perturbations.
 def insert_space(word):
@@ -102,6 +103,97 @@ def visual_similar_chars(word, *arg):
     perturbed_array = np.char.add(char_array, picked_unicode)
     return "".join(perturbed_array)
 
+
+def typo(word, probability):
+    '''
+    shifts a character by one keyboard space:
+    one space up, down, left or right
+
+    each word is typofied with some probability 'p': 
+    1. (p*100) percent of character will become typos
+
+    keyboard is defined as:
+    qwertyuiop
+    asdfghjkl
+     zxcvbnm
+
+    word = "Noise"
+    print(typo('Noise',0.1))
+    Noide
+
+    :param word : word to be shuffled
+    :param probability: probability of a typo
+
+    returns typofied word
+    '''
+    
+    # convert word to list (string is immutable)
+    word = list(word)
+    
+    num_chars_to_shift = math.ceil(len(word) * probability)
+    
+    # checking for capitalizations
+    capitalization = [False] * len(word)
+    
+    # convert to lowercase and record capitalization
+    for i in range(len(word)):
+        capitalization[i] = word[i].isupper()
+        word[i] = word[i].lower()
+    
+    # list of characters to be switched
+    positions_to_shift = []
+    for i in range(num_chars_to_shift):
+        positions_to_shift.append(random.randint(0, len(word) - 1))
+    
+    # defining a dictionary of keys located close to each character
+    keys_in_proximity = {
+        'a': ['q','w','s','x','z'],
+        'b': ['v','g','h','n'],
+        'c': ['x','d','f','v'],
+        'd': ['s','e','r','f','c','x'],
+        'e': ['w','s','d','r'],
+        'f': ['d','r','t','g','v','c'],
+        'g': ['f','t','y','h','b','v'],
+        'h': ['g','y','u','j','n','b'],
+        'i': ['u','j','k','o'],
+        'j': ['h','u','i','k','n','m'],
+        'k': ['j','i','o','l','m'],
+        'l': ['k','o','p'],
+        'm': ['n','j','k','l'],
+        'n': ['b','h','j','m'],
+        'o': ['i','k','l','p'],
+        'p': ['o','l'],
+        'q': ['w','a','s'],
+        'r': ['e','d','f','t'],
+        's': ['w','e','d','x','z','a'],
+        't': ['r','f','g','y'],
+        'u': ['y','h','j','i'],
+        'v': ['c','f','g','v','b'],
+        'w': ['q','a','s','e'],
+        'x': ['z','s','d','c'],
+        'y': ['t','g','h','u'],
+        'z': ['a','s','x']
+    }
+    
+    # insert typo
+    for pos in positions_to_shift:
+        # no typo insertion for special characters
+        try:
+            typo_list = keys_in_proximity[word[pos]]
+            word[pos] = random.choice(typo_list)
+        except:
+            break
+    
+    # reinsert capitalization
+    for i in range(len(word)):
+        if (capitalization[i]):
+            word[i] = word[i].upper()
+    
+    # recombine
+    word = ''.join(word)
+    
+    return word
+    
 
 if __name__ == "__main__":
     print(visual_similar_chars("adversarial"))
