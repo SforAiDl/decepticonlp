1890debb608fff1f55f63ec849bb2919d865ffc2
Somesh Kumar Singh <f20180175@goa.bits-pilani.ac.in>
2020-05-10 20:38:35 +0530

    Tests Complete

--- tests/test_perturb.py
+++ tests/test_perturb.py
@@ -11,16 +11,59 @@ from decepticonlp.transforms import perturb
     "word, expected_result",
     [
         ("Bob", "Bb"),
-        ("Never odd or even", "Never odd o even"),
-        ("Do geese see God?", "Do geese se God?"),
-        ("Elon Musk names his kid.", "Elon Musk names his kd."),
+        ("Hey there","Hey there"),
+        ("H","H"),
     ],
 )
 def test_perturb_delete(word, expected_result):
     random.seed(42)
     assert perturb.delete(word) == expected_result
 
-
 def test_perturb_delete_with_character_size_less_than_three():
     with pytest.raises(AssertionError):
-        perturb.delete("To")
+        perturb.delete("To", ignore=False)
+
+def test_perturb_delete_with_whitespace():
+    with pytest.raises(AssertionError):
+        perturb.delete("is wrong", ignore=False)
+
+@pytest.mark.parametrize(
+    "word, expected_result",
+    [
+        ("Bob", "B ob"),
+        ("Hey there","Hey there"),
+        ("H","H"),
+    ],
+)
+def test_perturb_insert_space(word, expected_result):
+    random.seed(42)
+    assert perturb.insert_space(word) == expected_result
+
+def test_perturb_insert_space_with_whitespace():
+    with pytest.raises(AssertionError):
+        perturb.insert_space("is wrong", ignore=False)
+
+def test_perturb_insert_space_with_character_size_less_than_two():
+    with pytest.raises(AssertionError):
+        perturb.insert_space("H", ignore=False)
+
+
+@pytest.mark.parametrize(
+    "word, expected_result",
+    [
+        ("EXTRA", "ETXRA"),
+        ("Hey there","Hey there"),
+        ("H","H"),
+    ],
+)
+def test_perturb_swap(word, expected_result):
+    random.seed(42)
+    assert perturb.swap(word) == expected_result
+
+def test_perturb_swap_with_character_size_less_than_four():
+    with pytest.raises(AssertionError):
+        perturb.swap("Ton")
+
+def test_perturb_swap_with_whitespace():
+    with pytest.raises(AssertionError):
+        perturb.swap("is wrong")
