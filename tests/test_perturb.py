3d44b967c93dd8512615e693d1a9be6e3b1da94c
Somesh Kumar Singh <f20180175@goa.bits-pilani.ac.in>
2020-05-10 20:38:49 +0530

    Tests Complete

--- tests/test_perturb.py
+++ tests/test_perturb.py
@@ -8,40 +8,35 @@ from decepticonlp.transforms import perturb
 
 
 @pytest.mark.parametrize(
-    "word, expected_result",
-    [
-        ("Bob", "Bb"),
-        ("Hey there","Hey there"),
-        ("H","H"),
-    ],
+    "word, expected_result", [("Bob", "Bb"), ("Hey there", "Hey there"), ("H", "H"),],
 )
 def test_perturb_delete(word, expected_result):
     random.seed(42)
     assert perturb.delete(word) == expected_result
 
+
 def test_perturb_delete_with_character_size_less_than_three():
     with pytest.raises(AssertionError):
         perturb.delete("To", ignore=False)
+
 
 def test_perturb_delete_with_whitespace():
     with pytest.raises(AssertionError):
         perturb.delete("is wrong", ignore=False)
 
+
 @pytest.mark.parametrize(
-    "word, expected_result",
-    [
-        ("Bob", "B ob"),
-        ("Hey there","Hey there"),
-        ("H","H"),
-    ],
+    "word, expected_result", [("Bob", "B ob"), ("Hey there", "Hey there"), ("H", "H"),],
 )
 def test_perturb_insert_space(word, expected_result):
     random.seed(42)
     assert perturb.insert_space(word) == expected_result
 
+
 def test_perturb_insert_space_with_whitespace():
     with pytest.raises(AssertionError):
         perturb.insert_space("is wrong", ignore=False)
+
 
 def test_perturb_insert_space_with_character_size_less_than_two():
     with pytest.raises(AssertionError):
@@ -50,19 +45,17 @@ def test_perturb_insert_space_with_character_size_less_than_two():
 
 @pytest.mark.parametrize(
     "word, expected_result",
-    [
-        ("EXTRA", "ETXRA"),
-        ("Hey there","Hey there"),
-        ("H","H"),
-    ],
+    [("EXTRA", "ETXRA"), ("Hey there", "Hey there"), ("H", "H"),],
 )
 def test_perturb_swap(word, expected_result):
     random.seed(42)
     assert perturb.swap(word) == expected_result
 
+
 def test_perturb_swap_with_character_size_less_than_four():
     with pytest.raises(AssertionError):
         perturb.swap("Ton")
+
 
 def test_perturb_swap_with_whitespace():
     with pytest.raises(AssertionError):
