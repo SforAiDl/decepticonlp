37d7acf49c4e00ce287be30a18678494c06b62ee
Parantak Singh <parantak.axilblaze@gmail.com>
2020-05-11 13:54:51 +0530

    Update shuffle perturb
    Coverage error fix | Attempt 1

--- decepticonlp/transforms/perturb.py
+++ decepticonlp/transforms/perturb.py
@@ -73,7 +73,7 @@ def shuffle(word, mid=True, ignore=True):
         random.shuffle(mid)
 
         return first + "".join(mid) + last
-    else:
+    elif mid==False:
         charlist = list(word)
         index = random.randint(1, len(word) - 3)  # select random offset for tuple
         charlist[index], charlist[index + 1] = (
