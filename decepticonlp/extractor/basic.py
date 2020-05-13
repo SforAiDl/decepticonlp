891fb3a865a138d52897dea61309343251f231b7
Somesh Kumar Singh <f20180175@goa.bits-pilani.ac.in>
2020-05-14 00:25:22 +0530

    Empty List Error, Minor Fixes

--- decepticonlp/extractor/basic.py
+++ decepticonlp/extractor/basic.py
@@ -1,5 +1,3 @@
-_AUTHOR_ = "SOMESH"
-
 import abc
 import random
 
@@ -14,9 +12,15 @@ class ImportantWordExtractor(metaclass=abc.ABCMeta):
     """
 
     @abc.abstractmethod
-    def extract(self, words: list, top_k: int, **kwargs):
-        """Applies perturbation and returns the word."""
+    def extract(self, words: list, **kwargs):  # pragma: no cover
+        """Extracts Important Word and returns the Index."""
         raise NotImplementedError
+
+    def empty_error_msg(self):  # pragma: no cover
+        return "given list of words is empty or invalid"
+
+    def word_less_than_k(self):  # pragma: no cover
+        return "number of words should be greater than top_k"
 
 
 class RandomImportantWordExtractor(ImportantWordExtractor):
@@ -24,9 +28,16 @@ class RandomImportantWordExtractor(ImportantWordExtractor):
         A class used to apply random word extractor.
         Methods
         -------
-        extract(self, word: list, **kwargs)
+        extract(self, words: list, top_k = 1, **kwargs)
             - extracts top_k random words and returns their index.
+        :params:
+        :words: - List of Words
+        :top_k: - Number of words to be extracted
     """
 
     def extract(self, words: list, top_k=1, **kwargs):
+
+        assert len(words) > 0, self.empty_error_msg()
+        assert len(words) >= top_k, self.word_less_than_k()
+
         return random.sample(range(len(words)), top_k)
--- tests/test_extractor_basic.py
+++ tests/test_extractor_basic.py
@@ -1,5 +1,3 @@
-_author_ = "Somesh Singh"
-
 import random
 import pytest
 from decepticonlp.extractor import basic
@@ -9,24 +7,28 @@ from decepticonlp.extractor import basic
     "words, expected_result",
     [(["Hey", "There", "Somesh"], [1]), (["Hey", "there"], [1])],
 )
-def test_perturb_delete(words, expected_result):
+def test_random_extract(words, expected_result):
     random.seed(0)
     random_extractor = basic.RandomImportantWordExtractor()
     assert random_extractor.extract(words) == expected_result
 
 
 @pytest.mark.parametrize(
-    "words, expected_result", [(["Raju", "Is", "NLP", "GAWD"], [3, 0])]
+    "words, expected_result", [(["Raju", "Is", "NLP", "GAWD"], [3, 1])]
 )
-def test_perturb_delete(words, expected_result):
+def test_random_k_extract(words, expected_result):
     random.seed(0)
     random_extractor = basic.RandomImportantWordExtractor()
     assert random_extractor.extract(words, top_k=2) == expected_result
 
 
-"""
-def test_perturb_delete_with_character_size_less_than_three():
-    delete_perturbations = perturbations.DeleteCharacterPerturbations()
+def test_random_empty_extract():
+    random_extractor = basic.RandomImportantWordExtractor()
     with pytest.raises(AssertionError):
-        delete_perturbations.apply("To", ignore=False)
-"""
+        random_extractor.extract([])
+
+
+def test_words_less_than_k_extract():
+    random_extractor = basic.RandomImportantWordExtractor()
+    with pytest.raises(AssertionError):
+        random_extractor.extract(["Hey", "There"], top_k=3)
