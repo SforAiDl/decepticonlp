2a85cd60f5eda5673eefaaeafbd9e577597c2f08
Parantak Singh <parantak.axilblaze@gmail.com>
2020-05-20 13:02:46 +0530

    Update with black formatting!

--- tests/test_char_metrics.py
+++ tests/test_char_metrics.py
@@ -39,12 +39,12 @@ def test_levenshtein_lcs(text1, text2, expected_result):
 
 
 @pytest.mark.parametrize(
-    "text1, text2, expected_result",
-    [("a cat", "an abct", 3), ("a cat", "a tc", 2)],
+    "text1, text2, expected_result", [("a cat", "an abct", 3), ("a cat", "a tc", 2)],
 )
 def test_damerau_levenshtein(text1, text2, expected_result):
     damerau_levenshtein_distance = char_metrics.DamerauLevenshtein()
     assert damerau_levenshtein_distance.calculate(text1, text2) == expected_result
+
 
 @pytest.mark.parametrize(
     "text1, text2, expected_result",
@@ -55,6 +55,7 @@ def test_damerau_levenshtein_lcs(text1, text2, expected_result):
     err = damerau_levenshtein_distance.calculate(text1, text2, "lcs") - expected_result
     assert -1e-5 < err < 1e-5
 
+
 @pytest.mark.parametrize(
     "text1, text2, expected_result",
     [("a cat", "an abct", 0.25), ("a cat", "a tc", 0.2222222222222222)],
@@ -63,6 +64,7 @@ def test_damerau_levenshtein_sum(text1, text2, expected_result):
     damerau_levenshtein_distance = char_metrics.DamerauLevenshtein()
     err = damerau_levenshtein_distance.calculate(text1, text2, "sum") - expected_result
     assert -1e-5 < err < 1e-5
+
 
 @pytest.mark.parametrize(
     "text1, text2, expected_result",
