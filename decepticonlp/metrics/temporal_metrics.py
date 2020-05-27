2a00a8ed6b5521e2d9fd94eb94264660fc96a3b8
hpandey680 <hpandey@merkleinc.com>
2020-05-27 18:51:29 +0530

    User defined Models

--- decepticonlp/metrics/temporal_metrics.py
+++ decepticonlp/metrics/temporal_metrics.py
@@ -8,15 +8,15 @@ class RankWords:
         inputs of shape (number of words,feature_vector_size)
         Methods
         --------
-        temporal_score(self, sentence)
+        temporal_score(self, model, sentence)
             - calculates the temporal score each word in a sentence(considering the first i words) 
-        temportal_tail_score(self, sentence)
+        temportal_tail_score(self, model, sentence)
             - calculates the temporal score each word in a sentence(considering n-i words)
-        combined_score(self,sentence,lambda_)
+        combined_score(self, model, sentence, lambda_)
             - calculates the combined score taking into consideration temporal score and tailed temporal score with lambda as the weight
     """
 
-    def temporal_score(self, sentence):
+    def temporal_score(self, model, sentence):
         """ 
             Considering a input sequence x1,x2,...,xn
             we calculate T(xi) = F(x1,x2,...,xi) - F(x1,x2,...,xi-1)
@@ -27,30 +27,30 @@ class RankWords:
             >> import temporal_metrics
             >> rw = temporal_metrics.RankWords()
             >> embedded_word = embed("i like dogs") #embed should convert the sentence to a (sent_length * embedding_dim) shape tensor
-            >> print(rw.temporal_score(embedded_word))  #seed = 99
+            >> print(rw.temporal_score(model,embedded_word))  #seed = 99 
             tensor([[1.1921e-07],[2.2054e-05],[8.4886e-01]])
 
             :params
+            :model : The model must output torch tensor of dimension (1,1,1) as its output
             :sentence : An tensor of shape (sent_length * embedding_dim) where each word has been embedded 
 
             returns temporal score of each word
             :return type: torch.Tensor
         """
         inputs = sentence.reshape(len(sentence), 1, -1)
-        model = nn.RNN(inputs.shape[2], 1)
         with torch.no_grad():
-            _, pred = model(inputs)
+            pred = model(inputs)
             losses = torch.zeros(inputs.shape[0:2])
             for i in range(inputs.size()[0]):
                 tempinputs = inputs.clone()
                 tempinputs[i, :, :].zero_()
-                _, tempoutput = model(tempinputs)
+                tempoutput = model(tempinputs)
                 losses[i] = torch.dist(
                     tempoutput.squeeze(1), pred.squeeze(1)
                 )  # L2 Norm
         return losses
 
-    def temportal_tail_score(self, sentence):
+    def temportal_tail_score(self, model, sentence):
         """ 
             Considering a input sequence x1,x2,...,xn
             we calculate T(xi) = F(xi,xi+1,...,xn) - F(xi+1,xi+2...,xn)
@@ -61,28 +61,28 @@ class RankWords:
             >> import temporal_metrics
             >> rw = temporal_metrics.RankWords()
             >> embedded_word = embed("i like dogs") #embed should convert the sentence to a (sent_length * embedding_dim) shape tensor
-            >> print(rw.temportal_tail_score(embedded_word)) #seed = 99
+            >> print(rw.temportal_tail_score(model,embedded_word)) #seed = 99
 
             tensor([[5.9605e-08],[1.9789e-05],[0.0000e+00]])
 
             :params
+            :model : The model must output torch tensor of dimension (1,1,1) as its output
             :sentence : An tensor of shape (sent_length * embedding_dim) where each word has been embedded 
 
             returns temporal score of each word
             :return type: torch.Tensor
         """
         inputs = sentence.reshape(len(sentence), 1, -1)
-        model = nn.RNN(inputs.shape[2], 1)
         losses = torch.zeros(inputs.shape[0:2])
         with torch.no_grad():
             for i in range(inputs.size()[0] - 1):
-                _, pred = model(inputs[i:, :, :])
+                pred = model(inputs[i:, :, :])
                 tempinputs = inputs[i + 1 :, :,].clone()
-                _, tempoutput = model(tempinputs)
+                tempoutput = model(tempinputs)
                 losses[i] = torch.dist(tempoutput, pred)  # L2 Norm
         return losses
 
-    def combined_score(self, sentence, lambda_):
+    def combined_score(self, model, sentence, lambda_):
         """ 
             Computes combined temporal_score, temportal_tail_score
             Combined Score = temporal_score + λ(temportal_tail_score)
@@ -92,17 +92,18 @@ class RankWords:
             >> import temporal_metrics
             >> rw = temporal_metrics.RankWords()
             >> embedded_word = embed("i like dogs") #embed should convert the sentence to a (sent_length * embedding_dim) shape tensor
-            >> print(rw.combined_score(embedded_word,0.5)) #seed = 99
+            >> print(rw.combined_score(model,embedded_word,0.5)) #seed = 99
 
             tensor([[6.8545e-07], [3.8507e-03], [8.4886e-01]])
 
             :params
+            :model : The model must output torch tensor of dimension (1,1,1) as its output
             :sentence : An tensor of shape (sent_length * embedding_dim) where each word has been embedded 
             :lambda_ : (float) , 0 <= lambda_ <= 1 
 
             returns temporal score of each word
             :return type: torch.Tensor
         """
-        return self.temporal_score(sentence) + lambda_ * self.temportal_tail_score(
-            sentence
-        )
+        return self.temporal_score(
+            model, sentence
+        ) + lambda_ * self.temportal_tail_score(model, sentence)
--- tests/test_temporal_score.py
+++ tests/test_temporal_score.py
@@ -4,6 +4,7 @@ import torch
 import pytest
 from decepticonlp.metrics import temporal_metrics
 import numpy as np
+import torch.nn as nn
 
 # GLOVE vectors consisting of the words that are used in this test function
 import json
@@ -18,6 +19,16 @@ def sentence_to_tensor(text):
     for i, word in enumerate(text.split(" ")):
         op[i] = torch.tensor(model_gigaword[word])
     return op
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(RNN, self).__init__()
+        self.rnn = nn.RNN(input_size, hidden_size)
+
+    def forward(self, x):
+        _, pred = self.rnn(x)
+        return pred
 
 
 @pytest.mark.parametrize(
@@ -35,8 +46,11 @@ def sentence_to_tensor(text):
 def test_temporal_score(text, expected_result):
     torch.manual_seed(99)
     text = sentence_to_tensor(text)
+    model = RNN(text.shape[1], 1)
     rw = temporal_metrics.RankWords()
-    assert all(torch.isclose(expected_result, rw.temporal_score(text), rtol=1e-4))
+    assert all(
+        torch.isclose(expected_result, rw.temporal_score(model, text), rtol=1e-4)
+    )
 
 
 @pytest.mark.parametrize(
@@ -46,19 +60,22 @@ def test_temporal_score(text, expected_result):
 def test_tailed_temporal_score(text, expected_result):
     torch.manual_seed(99)
     text = sentence_to_tensor(text)
+    model = RNN(text.shape[1], 1)
     rw = temporal_metrics.RankWords()
-    assert all(torch.isclose(expected_result, rw.temportal_tail_score(text), rtol=1e-4))
+    assert all(
+        torch.isclose(expected_result, rw.temportal_tail_score(model, text), rtol=1e-4)
+    )
 
 
 @pytest.mark.parametrize(
     "text,lambda_,expected_result",
     [
-        ("i like dogs", 0.5, torch.tensor([[6.8545e-07], [3.8507e-03], [8.4886e-01]])),
+        ("i like dogs", 0.5, torch.tensor([[1.4901e-07], [3.1948e-05], [8.4886e-01]])),
         (
             "he ran across the street",
             0.5,
             torch.tensor(
-                [[1.7881e-07], [3.6359e-06], [3.8207e-05], [1.5730e-01], [9.7338e-01]]
+                [[2.6822e-07], [5.8115e-06], [4.7475e-05], [2.1394e-01], [9.7338e-01]]
             ),
         ),
     ],
@@ -66,7 +83,10 @@ def test_tailed_temporal_score(text, expected_result):
 def test_combined_temporal_score(text, lambda_, expected_result):
     torch.manual_seed(99)
     text = sentence_to_tensor(text)
+    model = RNN(text.shape[1], 1)
     rw = temporal_metrics.RankWords()
     assert all(
-        torch.isclose(expected_result, rw.combined_score(text, lambda_), rtol=1e-4)
+        torch.isclose(
+            expected_result, rw.combined_score(model, text, lambda_), rtol=1e-4
+        )
     )
